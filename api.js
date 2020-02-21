a1e8618986334d4d3e772e7034cf079b4e8d575a
Ryan Compton <ryan.wade.compton@gmail.com>
2020-02-21 00:25:37 -0700

    Wire api.js & index.js. Remove electron?

--- api.js
+++ api.js
@@ -1,8 +1,34 @@
+const axios = require("axios");
+
+require("dotenv").config();
 const api = {
     getUser(username) {
-  
+        return axios
+            .get(
+                `https://api.github.com/users/${username}?client_id=${
+                process.env.CLIENT_ID
+                }&client_secret=${process.env.CLIENT_SECRET}`
+            ).catch(err => {
+                console.log("User undefined.");
+                process.exit(1);
+            });
+    },
+
+    getStars(username) {
+        return axios
+        .get(
+            `https://api.github.com/users/${username}/repos?client_id=${
+              process.env.CLIENT_ID
+            }&client_secret=${process.env.CLIENT_SECRET}&per_page=100`
+          )
+            .then(response => {
+                return response.data.reduce((acc, curr) => {//acc holds value of all prev. indexes while current iteration becomes index current.
+                    acc += curr.stargazers_count;
+                    return acc;
+                }, 0);
+
+            });
     }
-  };
-  
-  module.exports = api;
-  
+};
+
+module.exports = api;
--- index.js
+++ index.js
@@ -1,12 +1,62 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const open = require("open");
+const convertFactory = require("html-pdf");
+var options = { format: 'Letter' };
+const api = require("./api");
+const generateHTML = require("./generateHTML");
 const questions = [
-  
+
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What's your gitHub user name?"
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color??",
+        choices: ["red", "blue", "green", "pink"]
+    }
+
 ];
 
-function writeToFile(fileName, data) {
- 
-}
+// function writeToFile(fileName, data) {
+
+// }
 
 function init() {
-    
+    inquirer.prompt(questions).then(({ gitHub, color }) => {
+        console.log("Searching...");
+
+        api
+            .getUser(gitHub).then(response =>
+                api.getStars(gitHub).then(stars => {
+                    return generateHTML({
+                        stars,
+                        color,
+                        ...response.data
+                    });
+                })
+            )
+            .then(html => {
+                // console.log(html);
+                // const convert = convertFactory({
+                //     converterPath: convertFactory.converters.PDF
+                // });
+                convertFactory.create(html, options).toFile('./profile.pdf', function (err, result) {
+                    console.log(result);
+                    if (err) {
+                        return console.log(err);
+                    }
+                //     result.stream.pipe(//THIS LINE THROUGH 56 is unncessary!
+                //         fs.createWriteStream(path, join("./", "profile.pdf"))
+                //     );
+                // convert.kill();
+                });
+                // open(path.join(process.cwd(), "profile.pdf"))
+            })
+    })
 }
 init();
